0d7f6ddbb9602b0281fc7519ce6cc06e4018ffc6
Rodolfo Dicastro <rodolfodicastro@yahoo.com.br>
2021-10-05 22:17:48 -0300

    implementação endpoin pesquisar

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.get('/compras', (req, res) => {
-  res.send('Hello World!')
+var naoEncontrado = {
+  dataCompra: 'Usuário não encontrado!',
+  pontos: '0'
+}
+
+var mockData=[
+  {
+      id:10,
+      dataCompra: "05/11/2021",
+      pontos: 1
+  },
+  {
+      id:20,
+      dataCompra: "10/11/2021",
+      pontos: 1
+  },
+  {
+      id:30,
+      dataCompra: "14/11/2021",
+      pontos: 1
+  }
+]
+
+app.use(cors())
+app.get('/compras/:id', (req, res) => {
+  console.log(req.params.id)
+  var idSolicitado = req.params.id
+  var compraEncontrada = mockData.find(compra =>{
+    return compra.id == idSolicitado
+  })
+  console.log(compraEncontrada)
+  res.send(compraEncontrada ?? naoEncontrado) 
+
 })
 
 app.listen(port, () => {
